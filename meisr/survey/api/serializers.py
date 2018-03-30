d853a8055dd2306aa6678b0f6e29791bf78a138f
Sung <14kskim@gmail.com>
2018-03-29 20:41:39 -0500

    fixed question serializer to show correct domains/outcome

--- meisr/survey/api/serializers.py
+++ meisr/survey/api/serializers.py
@@ -1,17 +1,29 @@
 from rest_framework import serializers
-from survey.models import Question
-from survey.models import Answer
+from survey.models import *
 
 class QuestionSerializer(serializers.ModelSerializer):
+	section = serializers.SerializerMethodField()
+	func = serializers.SerializerMethodField()
+	dev = serializers.SerializerMethodField()
+	out = serializers.SerializerMethodField()
+
 	class Meta:
 		model = Question
 		fields = ('id','question_text', 'starting_age', 'section', 'func', 'dev', 'out')
+	def get_section(self, obj):
+		return obj.get_section_display()
+	def get_func(self, obj):
+		return(FunctionalDomain.objects.filter(question=obj.id).values_list('choice', flat=True))
+	def get_dev(self, obj):
+		return(DevelopmentalDomain.objects.filter(question=obj.id).values_list('choice', flat=True))
+	def get_out(self, obj):
+		return(Outcome.objects.filter(question=obj.id).values_list('choice', flat=True))
+
 
 class AnswerSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Answer
 		fields = ('id', 'user', 'question', 'rating')
-		read_only_fields = ['user']
 
 	def perform_create(self, serializer):
 		serializer.save(user=self.request.user)
