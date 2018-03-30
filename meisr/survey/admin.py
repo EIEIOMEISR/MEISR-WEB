16b7e471fda3d70c8c9ce1a08d1fe62d074d34e2
Sung <14kskim@gmail.com>
2018-03-30 00:50:46 -0500

    added domains/outcome as inlines to questions in admin site

--- meisr/survey/admin.py
+++ meisr/survey/admin.py
@@ -1,40 +1,43 @@
 from django.contrib import admin
 
-'''
-from .models import Question
-from .models import Answer
-from .models import Child
-from .models import FunctionalDomain
-from .models import DevelopmentalDomain
-from .models import Outcome
-'''
 from .models import *
 
-class QuestionAdmin(admin.ModelAdmin):
-	list_display = [field.name for field in Question._meta.fields]
+class FunctionalDomainInline(admin.TabularInline):
+	model = FunctionalDomain
 
+class DevelopmentalDomainInline(admin.TabularInline):
+	model = DevelopmentalDomain
+
+class OutcomeInline(admin.TabularInline):
+	model = Outcome
+
+class QuestionAdmin(admin.ModelAdmin):
+	inlines = [FunctionalDomainInline, DevelopmentalDomainInline, OutcomeInline,]
+	list_display = [field.name for field in Question._meta.fields]
+admin.site.register(Question, QuestionAdmin)
 class AnswerAdmin(admin.ModelAdmin):
 	list_display = [field.name for field in Answer._meta.fields]
-
+admin.site.register(Answer, AnswerAdmin)
 class ChildAdmin(admin.ModelAdmin):
 	list_display = [field.name for field in Child._meta.fields]
-
+admin.site.register(Child, ChildAdmin)
 class FunctionalDomainAdmin(admin.ModelAdmin):
 	list_display = [field.name for field in FunctionalDomain._meta.fields]
-
+admin.site.register(FunctionalDomain, FunctionalDomainAdmin)
 class DevelopmentalDomainAdmin(admin.ModelAdmin):
 	list_display = [field.name for field in DevelopmentalDomain._meta.fields]
-
+admin.site.register(DevelopmentalDomain, DevelopmentalDomainAdmin)
 class OutcomeAdmin(admin.ModelAdmin):
 	list_display = [field.name for field in Outcome._meta.fields]
-
+admin.site.register(Outcome, OutcomeAdmin)
 class ScoreAdmin(admin.ModelAdmin):
 	list_display = [field.name for field in Score._meta.fields]
-
+admin.site.register(Score, ScoreAdmin)
+'''
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Answer, AnswerAdmin)
 admin.site.register(Child, ChildAdmin)
 admin.site.register(FunctionalDomain, FunctionalDomainAdmin)
 admin.site.register(DevelopmentalDomain, DevelopmentalDomainAdmin)
 admin.site.register(Outcome, OutcomeAdmin)
-admin.site.register(Score, ScoreAdmin)
+admin.site.register(Score, ScoreAdmin)'''
