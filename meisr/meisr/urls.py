c5d30f6fe8c262c60db6b549bcdf2e772e95f1e0
Sung <14kskim@gmail.com>
2018-04-15 01:29:44 -0500

    fixed login redirect displaying wrong form

--- meisr/meisr/urls.py
+++ meisr/meisr/urls.py
@@ -3,8 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^', include('survey.urls'), name='survey'),
-    url(r'^accounts/', include('registration.backends.simple.urls')),
-    url(r'^', include('allauth.account.urls')),
+    url(r'^(accounts/)?', include('allauth.account.urls')),
     url(r'^api/', include('survey.api.urls'), name='api'),
     url(r'^admin/', admin.site.urls),
 ]
