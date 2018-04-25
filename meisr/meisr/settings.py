c47779a6acbbe87f3597702ebc51ae6b45acbe61
sung <14kskim@gmail.com>
2018-04-25 20:16:17 +0000

    prod settings

--- meisr/meisr/settings.py
+++ meisr/meisr/settings.py
@@ -155,7 +155,21 @@ REST_USE_JWT = True
 SITE_ID = 1
 
 # For registration
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+'''
+EMAIL_HOST = 'smtp.mailgun.org'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'postmaster@mg.meisr.org'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+EMAIL_USE_TLS = True
+'''
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'meisrtest@gmail.com'
+EMAIL_HOST_PASSWORD = 'one12345!'
+EMAIL_PORT = 587
+
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
